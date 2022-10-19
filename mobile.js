912f9ddb4347c6bba1d5bfb57157abc4a4ff5b3e
maarcio457 <110309311+maarcio457@users.noreply.github.com>
2022-10-19 19:39:57 -0300

    FinalizaçãoResponsivo

--- mobile.js
+++ mobile.js
@@ -1,5 +1,31 @@
 let key1 = document.querySelector(".key1")
+let key2 = document.querySelector(".key2")
+let key3 = document.querySelector(".key3")
+let key4 = document.querySelector(".key4")
+let key5 = document.querySelector(".key5")
+
+
 key1.addEventListener("touchstart", ()=>{
     let audio5 = document.querySelector(".audio5")
     audio5.play()
+});
+
+key2.addEventListener("touchstart", ()=>{
+    let audio3 = document.querySelector(".audio3")
+    audio3.play()
+});
+
+key3.addEventListener("touchstart", ()=>{
+    let audio4 = document.querySelector(".audio4")
+    audio4.play()
+});
+
+key4.addEventListener("touchstart", ()=>{
+    let audio1 = document.querySelector(".audio1")
+    audio1.play()
+});
+
+key5.addEventListener("touchstart", ()=>{
+    let audio2 = document.querySelector(".audio2")
+    audio2.play()
 });
